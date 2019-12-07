8da07ad8afa50860b0709e2cdac714528f82f770
송동욱 <quino0627@korea.ac.kr>
2019-12-08 00:20:49 +0900

    menu

---
+++ menuandfirstgame/Assets/SystemButtons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SystemButtons : MonoBehaviour
+{
+   public void GoToMenu()
+   {
+      SceneManager.LoadScene("Scenes/Menu");
+      
+   }
+}
